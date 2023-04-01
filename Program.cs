bee450b693cf8829bfbd464c5bc0213ed279e5b2
Adalyn Pop <popadalyn@gmail.com>
2023-04-01 09:33:23 -0600

    :rocket: Nueva app case

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//  VARIABLES
+int option;
+double A;
+double B;
+double resultado;
+string mensaje;
+
+//Dibujar Menu
+Console.WriteLine("***** CALCULADORA *****");
+Console.WriteLine("1. SUMAR.");
+Console.WriteLine("2. RESTAR.");
+Console.WriteLine("3. MULTIPLICAR.");
+Console.WriteLine("4. DIVIDIR.");
+Console.WriteLine("5. ELEVAR A LA POTENCIA.");
+Console.Write("SELECCIONE UNA OPCIÓN (1-5):");
